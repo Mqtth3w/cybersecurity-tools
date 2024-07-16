aa628a13b0ba10612b4154e567a0f49f3c10504f
Mqtth3w <mqtth3w@duck.com>
2024-07-16 12:33:46 +0200

    Add files via upload

--- sniffer.py
+++ sniffer.py
@@ -1,23 +1,16 @@
 from scapy.all import *
 
-# Funzione per catturare e analizzare i pacchetti di rete
 def packet_callback(packet):
     if packet.haslayer(IP):
         src_ip = packet[IP].src
         dst_ip = packet[IP].dst
         print(f"Packet: {src_ip} --> {dst_ip}")
-        
-        # Stampa dettagliata del pacchetto
         print(packet.show())
-        
-        # Stampa il contenuto "Raw" del pacchetto
         if packet.haslayer(Raw):
             print("Raw Data:")
-            print(packet[Raw].load.hex())  # Stampa il contenuto "Raw" come esadecimale
-            
-        print("=" * 50)  # Linea di separazione tra pacchetti
+            print(packet[Raw].load.hex())
+        print("=" * 60)
 
-# Avvia la cattura dei pacchetti in tempo reale
 def start_sniffing():
     sniff(prn=packet_callback, store=0)
 
