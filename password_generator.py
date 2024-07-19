adc22750b62eff659720d00a0ef742839396b70b
Mqtth3w <mqtth3w@duck.com>
2024-07-19 19:02:54 +0200

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,32 @@
+import itertools
+
+all_items = [ #user data as name, favorites stuff etc
+    "Vaorb", "Gakol", "Inception", "Matrix", "Avatar",
+    "Alice", "Bob", "Charlie", "Red", "4", "45",
+    "13/03/2020", "7 May", "tiger"
+]
+
+def gen_combinations(items, min_len=1, max_len=None):
+    if max_len is None:
+        max_len = len(items)
+    for r in range(min_len, max_len + 1):
+        for combo in itertools.combinations(items, r):
+            for perm in itertools.permutations(combo):
+                yield perm
+
+def generate_passwords(combinations):
+    for combo in combinations:
+        yield ''.join(combo)
+
+min_length = 2
+max_length = 5
+
+combinations_generator = gen_combinations(all_items, min_len=min_length, max_len=max_length)
+passwords_generator = generate_passwords(combinations_generator)
+
+count = 0
+for pwd in passwords_generator:
+    count += 1
+    print(pwd)
+
+print(count) #266,630
